3d284fd0ec0744b0da0acba9c450ceb44208142e
Joao Paulo Cavendish <joaopaulocavendish@gmail.com>
2020-04-23 08:57:32 -0300

    gobarber sign and sign up pages

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import SignInBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin: 60px auto;
+    width: 340px;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: flex;
+      flex-direction: column;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${SignInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
