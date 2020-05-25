00854cf5180f749f82d10d64259338889ea3034d
Joao Paulo Cavendish <joaopaulocavendish@gmail.com>
2020-05-24 23:22:41 -0300

    add dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,18 +1,24 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatarUrl: string;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthState {
-  user: object;
+  user: User;
   token: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -25,6 +31,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
     return {} as AuthState;
@@ -44,6 +52,9 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
     setData({ token, user });
   }, []);
 
